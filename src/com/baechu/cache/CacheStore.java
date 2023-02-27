41a1599388a2ea5ebcc6ed69295eb0e046c73758
baeseohyeon <formnarago@naver.com>
2023-02-27 19:04:28 +0900

    feat: CacheStore 추상클래스 추가

---
+++ src/com/baechu/cache/CacheStore.java
@@ -0,0 +1,28 @@
+package com.baechu.cache;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public abstract class CacheStore<K,V> {
+
+    protected final Map<K, V> map;
+    protected final DoubleLinkedList<K> nodes;
+
+    protected CacheStore(Map<K, V> map, DoubleLinkedList<K> nodes) {
+        this.map = map;
+        this.nodes = nodes;
+    }
+
+    public abstract void put(K key, V value);
+
+    public abstract V get(K key);
+
+    public abstract V remove(K key);
+
+    public abstract void deleteByLRU();
+
+    public abstract void clear();
+
+    public abstract Set<Entry<K, V>> findAll();
+}
